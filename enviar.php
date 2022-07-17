ed58246d0abec0d00ab19a36fa855166931075dd
Salvador <salvador.ambriz.maya@gmail.com>
2022-07-16 21:06:08 -0500

    php

--- enviar.php
+++ enviar.php
@@ -1,32 +1,27 @@
- <?php
+<?php
+$name = $_POST['name'];
+$mail = $_POST['mail'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
 
- $name = $_POST['name'];
- $phone = $_POST['phone'];
- $lastname = $_POST['lastname'];
- $mail = $_POST['mail'];
- $servicio = $_POST['servicio'];
- $message = $_POST['message'];
- $contacto = $_POST['contacto'];
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
 
- $header = 'From ' . $mail . " \r\n";
- $header .= "X-Mailer: PHP/ " . phpversion() . " \r\n";
- $header .= "Mime-Version: 1.0 \r\n";
- $header .= "Content-Type: text/plain";
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Teléfono de contacto: " . $phone . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
 
- $message = "Este mensaje fue enviado por " . $name . " \r\n";
- $message = "Su email es " . $mail . " \r\n";
- $message = "Teléfono de contacto " . $phone . " \r\n";
- $message = "Mensaje " . $_POST['message'] . " \r\n";
- $message = "Enviado el " . date('d/m/Y', time()) . " \r\n";
+$para = 'salvador.ambriz.mauya@gmail.com';
+$asunto = 'Mensaje de... (Escribe como quieres que se vea el remitente de tu correo)';
 
- $para = 'salvador.ambriz.maya@gmail.com';
- $asunto = 'Solicitud de cita'
+mail($para, $asunto, utf8_decode($message), $header);
 
- mail($para, $asunto, utf8_decode($message), $header);
-
- header("Location:index.html");
- ?>
-
+header("Location:index.html");
+?>
 
 
 
