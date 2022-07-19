5ac63284b1fe9e6e361c2e0d5a344cac79e4fb09
Salvador <salvador.ambriz.maya@gmail.com>
2022-07-18 21:24:24 -0500

    php

--- enviar.php
+++ enviar.php
@@ -1,8 +1,27 @@
 <?php
-$nombre = $_POST['name'];
-$telefono = $_POST['phone'];
+$name = $_POST['name'];
 $mail = $_POST['mail'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$service = $_POST['servicio'];
 
-mail('salvador.ambriz.maya@gmail.com', $nombre, $mail);
 
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su teléfono de contacto es: " . $phone . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "El servicio deseado es: " . $service . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+
+
+$para = 'salvador.ambriz.maya@gmail.com';
+$asunto = 'Mensaje de solicitud de contacto vía web de Integral Medic';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:home.html");
 ?>
