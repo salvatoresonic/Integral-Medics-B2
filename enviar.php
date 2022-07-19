f8cd6866c2cf28d833e020e81da0d5ee8a0aa4b3
Salvador <salvador.ambriz.maya@gmail.com>
2022-07-18 20:51:50 -0500

    php prueba

--- enviar.php
+++ enviar.php
@@ -1,27 +1,8 @@
 <?php
-$name = $_POST['name'];
+$nombre = $_POST['name'];
+$telefono = $_POST['phone'];
 $mail = $_POST['mail'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
 
-$header = 'From: ' . $mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+mail('salvador.ambriz.maya@gmail.com', $nombre, $mail);
 
-$message = "Este mensaje fue enviado por: " . $name . " \r\n";
-$message .= "Su e-mail es: " . $mail . " \r\n";
-$message .= "Teléfono de contacto: " . $phone . " \r\n";
-$message .= "Mensaje: " . $_POST['message'] . " \r\n";
-$message .= "Enviado el: " . date('d/m/Y', time());
-
-$para = 'salvador.ambriz.mauya@gmail.com';
-$asunto = 'Mensaje de... (Escribe como quieres que se vea el remitente de tu correo)';
-
-mail($para, $asunto, utf8_decode($message), $header);
-
-header("Location:index.html");
 ?>
-
-
-
