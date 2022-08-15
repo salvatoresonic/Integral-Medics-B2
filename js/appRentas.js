77580a38ce027010b65a40ae1b4401205684f75b
Salvador <salvador.ambriz.maya@gmail.com>
2022-08-14 21:35:28 -0500

    rentas responsive

---
+++ js/appRentas.js
@@ -0,0 +1,49 @@
+window.addEventListener("load", function () {
+  new Glider(this.document.querySelector(".carousel__lista"), {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    duration: 3.25,
+    rewind: true,
+    dots: ".carousel__indicadores",
+    arrows: {
+      prev: ".carousel__anterior__rentas",
+      next: ".carousel__siguiente__rentas",
+    },
+
+    responsive: [
+      {
+        // screens greater than >= 650px
+        breakpoint: 650,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          duration: 2.25,
+        },
+      },
+      {
+        // screens greater than >= 900px
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          duration: 2.25,
+        },
+      },
+      {
+        // screens greater than >= 1024px
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          duration: 2.25,
+        },
+      },
+    ],
+  });
+});
+
+//const btnAnt = document.querySelector('#carousel__anterior__btn')
+//const btnSig = document.querySelector('#carousel__siguiente__btn')
+//console.log(btnAnt)
